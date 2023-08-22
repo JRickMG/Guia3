f817672a41dc54450c06e1a48c96fb801dee95b9
Ricardo Mejía <Ricardomejia218@gmail.com>
2023-08-22 08:16:54 -0600

    second commit

---
+++ src/data.js
@@ -0,0 +1,39 @@
+export const data=[
+    {
+        id:1,
+        title:'Cien años de soledad',
+        price:100,
+        urlImage:'https://covers.alibrate.com/b/59872e8acba2bce50c1a6d96/b0bf30dd-8585-4a46-9229-c72a94282fbe/share',
+        quantity:1
+    },
+    {
+        id:2,
+        title:'El señor de los anillos(Trilogía)',
+        price:190,
+        urlImage:'https://global-uploads.webflow.com/60a78e52a3ba3a525a3b96cf/60af9b04c29dbd5de0abe92b_el-se%C3%B1or-de-los-anillos-trilogia.jpg',
+        quantity:1
+    },
+    {
+        id:3,
+        title:'Cuentos de barro',
+        price:30,
+        urlImage:'https://4.bp.blogspot.com/-D6DeGCmoHfE/U8XAw_N_h4I/AAAAAAAAAtQ/H4gdZkJ_o_o/s1600/cuentos+de+barro.PNG',
+        quantity:1
+    },
+    {
+        id:4,
+        title:'Tierra de infancia',
+        price:30,
+        urlImage:'https://www.libreriauca.com/system/balloom/product_assets/attachments/000/018/145/normal/LIB-9789992349199.jpg?1598834281',
+        quantity:1
+    },
+    {
+        id:5,
+        title:'Harry Potter Pack',
+        price:390,
+        urlImage:'https://m.media-amazon.com/images/S/aplus-media/vc/3c49445f-eaf2-499c-ad5f-b0a65459b664.__CR0,0,970,300_PT0_SX970_V1___.jpg',
+        quantity:1
+    }
+
+]
+
